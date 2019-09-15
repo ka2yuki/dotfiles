fabb1ca3efc19b0dfd85a786eba1a52de22ccd34
katsuyuki mori <mori1103k@gmail.com>
2019-09-15 22:29:15 +0900

    add: setting.json of VSCode

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,5 @@ ln -sf ~/d/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/d/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/d/dotfiles/.sqliterc ~/.sqliterc
 
-ln -sf ~/d/dotfiles/.gitconfig ~/.gitconfig 
+ln -sf ~/d/dotfiles/.gitconfig ~/.gitconfig
+ln -fnsv ~/d/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json 
