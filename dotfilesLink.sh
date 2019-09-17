ad568c5554105b29ebac108f78457130b8ae670b
katsuyuki mori <mori1103k@gmail.com>
2019-09-17 14:42:52 +0900

    iterm

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/d/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/d/dotfiles/.sqliterc ~/.sqliterc
 
 ln -sf ~/d/dotfiles/.gitconfig ~/.gitconfig
-ln -fnsv ~/d/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json 
+ln -fnsv ~/d/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
+# ln -sf ~/d/dotfiles/com.googlecode.iterm2.plist ~
